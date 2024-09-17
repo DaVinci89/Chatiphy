a916f834b4906f0b559be64a9eb7259e158c366a
DaVinci89 <davinci3712@gmail.com>
2024-09-17 19:59:37 +0300

    Creating Post Maker app structure

--- Chatiphy/Chatiphy/settings.py
+++ Chatiphy/Chatiphy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'post_maker.apps.PostMakerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
