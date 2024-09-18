7955baacefa84f60fc316f2ff3f22296b79894ef
DaVinci89 <davinci3712@gmail.com>
2024-09-18 20:20:40 +0300

    Adding urls and view functions

--- Chatiphy/Chatiphy/urls.py
+++ Chatiphy/Chatiphy/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include("post_maker.urls")),
+    path('group/<slug:slug>', include("post_maker.urls")),
     path('admin/', admin.site.urls),
 ]
--- Chatiphy/post_maker/views.py
+++ Chatiphy/post_maker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("CHATIPHY\nMain Page")
+
+def group_posts(request):
+    return HttpResponse("CHATIPHY\nPosts filtered by Groups")
