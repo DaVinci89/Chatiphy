76ee4527f9c9cd4529d0d460f03ff264f4a9d2b1
DaVinci89 <davinci3712@gmail.com>
2024-09-18 20:20:40 +0300

    Adding urls and view functions

--- Chatiphy/post_maker/views.py
+++ Chatiphy/post_maker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("CHATIPHY<br>Main Page")
+
+def group_posts(request, page):
+    return HttpResponse(f"CHATIPHY<br>Posts filtered by Groups<br>{page}")
