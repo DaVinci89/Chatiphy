da2770f446d4ffd7ee84ff6dfbe0ce4987b6af67
DaVinci89 <davinci3712@gmail.com>
2024-09-23 15:11:04 +0300

    Creating admin-zone, superuser, database and models for making posts.

--- Chatiphy/post_maker/admin.py
+++ Chatiphy/post_maker/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # List of models attributes shown at the interface(pk means PrimaryKey)
+    list_display = ("pk", "text", "pub_date", "author")
+    # List of fields for searching
+    search_fields = ("text",)
+    # List of fields for filtering
+    list_filter = ("pub_date",)
+    # Value for empty rows in the text fields
+    empty_value_display = "--empty--"
+
+
+admin.site.register(Post, PostAdmin)
