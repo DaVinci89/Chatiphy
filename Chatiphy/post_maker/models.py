13021292db6c6e2a5a86c49c55b614358253060f
DaVinci89 <davinci3712@gmail.com>
2024-09-26 23:18:01 +0300

    Adding representation for model Post. Reformatting models.py

--- Chatiphy/post_maker/models.py
+++ Chatiphy/post_maker/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.utils.text import slugify
 
 User = get_user_model()
-
-
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.CharField(max_length=150, blank=True, null=True)
 
 
 class Group(models.Model):
@@ -19,3 +11,13 @@ class Group(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    group = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.text
