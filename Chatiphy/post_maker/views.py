1a7cbe0614f86bea0c4a2c3af1482aabd28c9be3
DaVinci89 <davinci3712@gmail.com>
2024-10-03 18:52:22 +0300

    Adding paginator for post_maker app. Paginator now works on the main page, group list and post list in group.

--- Chatiphy/post_maker/views.py
+++ Chatiphy/post_maker/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+from django.core.paginator import Paginator
 
 
 def index(request):
     keyword = request.GET.get("q", None)
     if keyword:
-        posts = Post.objects.filter(text__contains=keyword).select_related("author")
+        posts = Post.objects.filter(text__contains=keyword).select_related("author").order_by("-pub_date")
     else:
-        posts = Post.objects.order_by("-pub_date")[:10]
+        posts = Post.objects.order_by("-pub_date")
     template = "post_maker/index.html"
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        "posts": posts,
+        "page_obj": page_obj,
     }
     return render(request, template, context)
 
@@ -18,11 +22,14 @@ def index(request):
 def group_posts(request):
     template = "post_maker/group_posts.html"
     text = "<h2><b>Groups</b><h2>"
-    groups = Group.objects.all()[:10]
+    groups = Group.objects.all()
     groups_count = Group.objects.count()
+    paginator = Paginator(groups, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         "header": text,
-        "groups": groups,
+        "page_obj":page_obj,
         "groups_count":groups_count
     }
     return render(request, template, context)
@@ -32,8 +39,11 @@ def group_posts_page(request, page):
     template = "post_maker/group_posts_page.html"
     group = get_object_or_404(Group, slug=page)
     posts = Post.objects.filter(group=group).order_by("-pub_date")
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         "group": group,
-        "posts": posts,
+        "page_obj":page_obj,
     }
     return render(request, template, context)
