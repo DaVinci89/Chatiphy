2dcaa2c1ef751532cda238c09d5e6b5ce0382bc7
DaVinci89 <davinci3712@gmail.com>
2024-10-03 22:33:08 +0300

    Adding functionality for feedback form. Now it is possible to send email for admin.

--- Chatiphy/post_maker/urls.py
+++ Chatiphy/post_maker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("groups/", views.group_posts, name="group_posts"),
     path("groups/<slug:page>", views.group_posts_page, name="group_page"),
-    path("feedback/", views.feedback, name="feedback")
+    path("feedback/", views.feedback, name="feedback"),
+    path("feedback/success/", views.feedback_success, name="feedback_success")
 ]
--- Chatiphy/post_maker/views.py
+++ Chatiphy/post_maker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.conf import settings
 from .models import Post, Group
 from django.core.paginator import Paginator
+from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
 from .forms import FeedbackForm
 
@@ -54,9 +56,20 @@ def feedback(request):
     if request.method == "POST":
         form = FeedbackForm(request.POST)
         if form.is_valid():
-            return redirect("post_maker/thank-you")
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subject = form.cleaned_data["subject"]
+            message = form.cleaned_data["body"]
+            send_mail( f'New Feedback Form {subject} from {name}',
+                f'Message: {message}\n\nFrom: {name} ({email})',
+                settings.DEFAULT_FROM_EMAIL,
+                ['workdavinci39@gmail.com'],
+                fail_silently=False)
+            return redirect("post_maker:feedback_success")
         return render(request, "post_maker/feedback.html", {"form":form})
     form = FeedbackForm()
     return render(request, "post_maker/feedback.html", {"form":form})
 
+def feedback_success(request):
+    return render(request, "post_maker/feedback_success.html")
 
