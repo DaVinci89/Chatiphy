c4fa4ec459926fccdca65e41d4b36cd2b0211d80
DaVinci89 <davinci3712@gmail.com>
2025-05-01 23:44:32 +0300

    Finished like/dislike. TODO: Serializers? Or something else...

--- Chatiphy/core/templatetags/filters.py
+++ Chatiphy/core/templatetags/filters.py
@@ -9,3 +9,11 @@ def add_class(field, css):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def count_likes(post):
+    return post.likes_dislikes.filter(is_like=True).count()
+
+@register.filter
+def count_dislikes(post):
+    return post.likes_dislikes.filter(is_like=False).count()
